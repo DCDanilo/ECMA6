0fdd3490d0897050442231908b4ec7871314637d
DCDanilo <119107956+DCDanilo@users.noreply.github.com>
2023-07-13 08:07:13 +0200

    test url

--- nodeJS/moduli/progettoFinaleModuli/server.js
+++ nodeJS/moduli/progettoFinaleModuli/server.js
@@ -1,34 +1,35 @@
-const http = require('http')
-const url = require('url')
+const http = require('http');
+const url = require('url');
 
-function creaPrezzo(prezzoPrecedente){
-    let valore = Math.round(prezzoPrecedente+(Math.random(10)*10-5))
-    return valore>0?valore:1;
-}
+// function creaPrezzo(prezzoPrecedente){
+//     let valore = Math.round(prezzoPrecedente+(Math.random(10)*10-5))
+//     return valore>0?valore:1;
+// }
 
-function caricaDati(res, nome, t){
-    let nuovoPrezzo = Math.round(Math.random()*50+20);
-    for(i=0; i<t-1; i++){
-        nuovoPrezzo=creaPrezzo(nuovoPrezzo);
-        res.write(`${nome}\t ${nuovoPrezzo}\n}`)
+// function caricaDati(res, nome, t){
+//     let nuovoPrezzo = Math.round(Math.random()*50+20);
+//     for(i=0; i<t-1; i++){
+//         nuovoPrezzo=creaPrezzo(nuovoPrezzo);
+//         res.write(`${nome}\t ${nuovoPrezzo}\n}`)
 
+//     }
+//     nuovoPrezzo=creaPrezzo(nuovoPrezzo);
+//     res.end(`${nome}\t ${nuovoPrezzo}\n}`);
+// }
+
+//http://localhost:8080/?t=20&n=Test
+
+const server = http
+  .createServer((req, res) => {
+    let query = url.parse(req.url).query;
+    if (query.n != undefined && query.t != undefined) {
+      res.writeHead(200);
+      // caricaDati(res,query.t, query.n)
+    } else {
+      res.writeHead(400);
+      res.end();
     }
-    nuovoPrezzo=creaPrezzo(nuovoPrezzo);
-    res.end(`${nome}\t ${nuovoPrezzo}\n}`);
-}
+  })
+  .listen('8080');
 
-const server = http.createServer((req, res) => {
-
-    let query = url.parse(req.url).query
-    if(query.n!=undefined && query.t!=undefined ){
-        res.writeHead(200)
-        caricaDati(res,query.t, query.n)
-    }
-    else{
-        res.writeHead(400);
-        res.end();
-    }
-}).listen('8080')
-
-
-console.log(`server started at ${server.address().port}` )
+console.log(`server started at ${server.address().port}`);
