6dfe99496f9d41271796566e3ea99c37b39503e9
DCDanilo <119107956+DCDanilo@users.noreply.github.com>
2023-07-14 08:31:08 +0200

    1 file modified, 2 added

---
+++ nodeJS/moduli/progettoFinaleModuli/client.js
@@ -0,0 +1,22 @@
+// const request = require('request');
+
+// request('http://localhost:8080/?t=20&n=Test', (error, response, body) => {
+//   if (error == null) {
+//     if (response && response.statusCode == 200) {
+//       console.log(body);
+//     } else {
+//       console.log('richiesta non valida');
+//     }
+//   } else {
+//     console.log('errore');
+//   }
+// });
+
+// var test = 'luigi;neri;Roma;38'.split(';');
+// console.log(test[1]);
+
+// var test = parseInt('5') + 6 + '5';
+// console.log(test);
+
+var test = Math.floor(Math.random());
+console.log(test);
