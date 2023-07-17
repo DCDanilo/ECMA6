c0c9ee33ed8eea797ad7a6253180f9d47c95b9a2
DCDanilo <danilo.decave@gmail.com>
2023-07-17 16:26:50 +0200

    fix

--- nodeJS/moduli/progettoFinaleModuli/server.js
+++ nodeJS/moduli/progettoFinaleModuli/server.js
@@ -1,13 +1,37 @@
-const http = require('http');
 const url = require('url');
+const http = require('http');
 
-function creaPrezzo(prezzoPrecedente){
-    let valore = Math.round(prezzoPrecedente+(Math.random(10)*10-5))
-    return valore>0?valore:1;
-}
+//http://localhost:8080?t=20&n=Test
+
+const server = http.createServer(
+    (req, res) => {
+        query = url.parse(req.url).query
+        if(query.t && query.n){
+            res.writeHead(200)
+            caricaDati(res,query.n,query.t)
+        }
+        else{
+            res.writeHead(400)
+            res. end()
+        }
+    }
+).listen('8080')
+
+console.log(`server started at ${server.address().port}`);
+
+
+
+
+
+
+
+// function creaPrezzo(prezzoPrecedente){
+//     let valore = Math.round(prezzoPrecedente+(Math.random(10)*10-5))
+//     return valore>0?valore:1;
+// }
 
 function caricaDati(res, nome, t){
-    let nuovoPrezzo = Math.round(Math.random()*50+20);
+    var nuovoPrezzo = Math.round(Math.random()*50+20);
     for(i=0; i<t-1; i++){
         nuovoPrezzo=creaPrezzo(nuovoPrezzo);
         res.write(`${nome}\t ${nuovoPrezzo}\n}`)
@@ -16,20 +40,3 @@ function caricaDati(res, nome, t){
     nuovoPrezzo=creaPrezzo(nuovoPrezzo);
     res.end(`${nome}\t ${nuovoPrezzo}\n}`);
 }
-
-//http://localhost:8080/?t=20&n=Test
-
-const server = http
-  .createServer((req, res) => {
-    let query = url.parse(req.url).query;
-    if (query.n != undefined && query.t != undefined) {
-      res.writeHead(200);
-      caricaDati(res,query.t, query.n)
-    } else {
-      res.writeHead(400);
-      res.end();
-    }
-  })
-  .listen('8080');
-
-console.log(`server started at ${server.address().port}`);
